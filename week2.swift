15a5b9d1fec97d828ef8168b605b07795a789980
AnnaAtmadjaja <>
2022-03-08 00:50:12 +0000

    Initial commit

---
+++ week2.swift
@@ -0,0 +1,14 @@
+var isBanned = false
+print(isBanned)
+
+var winRate: Double = 45.36
+print(winRate)
+
+winRate = 52.76
+print(winRate)
+
+let heroName: String = "coraline"
+print(heroName)
+
+let 🏹: String = "mark"
+print(🏹)
